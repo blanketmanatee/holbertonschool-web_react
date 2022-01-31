247c19294ecc000ff0a5da745dd73383e3bcaf00
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-31 12:52:02 -0600

    initial push

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,33 @@
+interface Student {
+    firstName: string
+    lastName: string
+    age: number
+    location: string
+}
+
+const studentsList: Array<Student> = [student1, student2];
+
+const body: HTMLBodyElement = document.getElementsByTagName("body") [0];
+const table: HTMLTableElement = document.createElement("table");
+const thead: HTMLTableSectionElement = document.createElement("thead");
+const tbody: HTMLTableSectionElement = document.createElement("tbody");
+
+const rowHead: HTMLTableRowElement = thread.insertRow(0);
+const cellHead: HTMLTableCellElement = rowHead.insertCell(0);
+const cell2Head: HTMLTableCellElement = rowHead.insertCell(1);
+
+cellHead.innerHTML = "firstName";
+cell2Head.innerHTML = "location";
+
+table.append(thead);
+
+studentsList.forEach((student) => {
+    const row: HTMLTableRowElement = tbody.insertRow(0);
+    const cell1: HTMLTableCellElement = row.insertCell(0);
+    const cell2: HTMLTableCellElement = row.insertCell(1);
+
+    cell1.innerHTML = student.firstName;
+    cell2.innerHTML = student.location;
+});
+table.append(tbody);
+body.append(table);
