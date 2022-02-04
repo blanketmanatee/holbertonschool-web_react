4d921889ced980b62b33a5003e36acdfcc9eb622
blanketmanatee <ashleyl.welch@gmail.com>
2022-02-03 19:03:17 -0600

    task 3

---
+++ 0x01-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import './body.css';
+import _ from 'lodash';
+
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+
+function updateCounter() {
+  let times = $('#count').html() || 0;
+  $('button').on('click', () => {
+    times++;
+    $('#count').html(`${times} clicks on the button`);
+  });
+};
+
+_.debounce(updateCounter(), 500);
