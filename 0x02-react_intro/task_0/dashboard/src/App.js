af0193881d872be179ae7dabe972e840ae4d9537
blanketmanatee <ashleyl.welch@gmail.com>
2022-02-12 20:36:39 -0600

    updated app.js

--- 0x02-react_intro/task_0/dashboard/src/App.js
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -6,18 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>School dashboard</h1>
       </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+      Copyright 2020 - holberton School
+      </footer>
     </div>
   );
 }
