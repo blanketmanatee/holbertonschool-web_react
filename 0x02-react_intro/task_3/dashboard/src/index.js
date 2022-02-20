2c345e581b7308b06f00d481fa66975f6fc948b4
Ashley Price <ashleyl.welch@gmail.com>
2022-02-20 16:25:40 +0000

    uploading task 3 corrections

---
+++ 0x02-react_intro/task_3/dashboard/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications';
+
+ReactDOM.render(
+  <React.StrictMode>
+  <div className="root-notifications">
+    <Notifications />
+  </div>
+  <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
