ca789c884e1e6d5c565f3f4fe68b1f5a60cfeb30
Ashley Price <ashleyl.welch@gmail.com>
2022-02-20 21:46:28 +0000

    current build working

--- 0x02-react_intro/task_3/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_3/dashboard/src/Notifications.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './Notifications.css';
-<<<<<<< HEAD
-=======
 import { getLatestNotification } from './utils';
-import close_icon from './close-icon.png';
->>>>>>> 2c345e581b7308b06f00d481fa66975f6fc948b4
+import close_Icon from './close-Icon.png';
 
 const Notifications = () => {
     return (
@@ -17,8 +14,13 @@ const Notifications = () => {
 	    <div dangerouslySetInnerHTML={{ __html: `${getLatestNotification()}` }} ></div> 
 	    </li>
             </ul>
-            <button type="button" aria-label="close" onClick={() => console.log('Close button has been clicked')} style={{dislay: 'inline-block', position: 'absolute', top: '16px', right: '16px', right: '16px', background: 0, border: 0, outline: 'none', cursor: 'pointer',}}>
-                <img src={close_icon} alt="" style={{ width: '8px', height: '8px' }} />
+            <button type="button" aria-label="close" onClick={() => console.log
+('Close button has been clicked')}
+style={{dislay: 'inline-block', 
+position: 'absolute', 
+top: '16px',
+right: '16px', background: 0, border: 0, outline: 'none', cursor: 'pointer',}}>
+                <img src={close_Icon} alt="" style={{ width: '8px', height: '8px' }} />
             </button>
         </div>
     );
