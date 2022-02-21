63044e263dc2596434f5bf47053b38421e7d34ce
Ashley Price <ashleyl.welch@gmail.com>
2022-02-21 04:30:51 +0000

    task 4

---
+++ 0x02-react_intro/task_4/dashboard/src/Notifications.test.js
@@ -0,0 +1,14 @@
+import { shallow } from 'enzyme';
+import Notifications from './Notifications';
+
+const wrapper = shallow(<Notifications />);
+
+it ('renders without crashing', () => {
+shallow(<Notifications />);
+});
+it ('renders three list items', () => {
+expect(wrapper.find('li').children().length).toEqual(3);
+});
+it('renders the <p>', () => {
+expect(wrapper.containsMatchingElement(<p>NHere is the list of notifications</p>)).toBeTruthy();
+});
---
+++ 0x02-react_intro/task_4/dashboard/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App';
+import Notifications from './Notifications';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+  <div className="root-notifications">
+    <Notifications />
+  </div>
+  <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
