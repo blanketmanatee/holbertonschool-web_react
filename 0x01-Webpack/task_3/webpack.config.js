770e822726cd2a956d2420751804036d70d97449
blanketmanatee <ashleyl.welch@gmail.com>
2022-04-16 00:36:12 -0500

    updated config

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -1,34 +1,50 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry : path.resolve(__dirname, 'js/dashboard_main.js'),
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
+  mode: 'development',
+  entry : path.resolve(__dirname, 'js/dashboard_main.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
     },
-    performance: {
+  },
+  performance: {
     maxAssetSize: 1000000,
   },
-    module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(jpg|gif|png|jpeg|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
+  devServer: {
+    contentBase: path.join(__dirname, './public'),
+    compress: true,
+    port: 8564,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'public/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    },
+    {
+      test: /\.(jpg|gif|png|jpeg|svg)$/i,
+      use: [
+        "file-loader",
+        {
+          loader: "image-webpack-loader",
+          options: {
+            bypassOnDebug: true,
+            disable: true,
           },
-        ],
-      }
-    ]
-  }
+        },
+      ],
+    }
+  ]
+}
 }
