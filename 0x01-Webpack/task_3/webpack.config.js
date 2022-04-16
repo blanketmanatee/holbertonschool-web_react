37c50c7e1616ddfcf1d1326ef8b06b7f9f125ce6
blanketmanatee <ashleyl.welch@gmail.com>
2022-04-16 00:44:29 -0500

    updated config

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
   optimization: {
     splitChunks: {
